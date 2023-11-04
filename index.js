f065190b2618c696d4f6dcf20dd2c3977fb82bbf
maddie0202 <148143460+maddie0202@users.noreply.github.com>
2023-11-04 13:04:49 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+function showTemperature(response) {
+  let tempEl = document.getElementById("int");
+  let temp = Math.round(response.data.daily[now.getDay()].temperature.day);
+  cityName.innerHTML = response.data.city;
+  tempEl.innerHTML = temp;
+}
+
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.getElementById("cityInput").value;
+  let apiKey = "35obt3d4a77e941e006826ae0feb45c4";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityInput}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let today = days[now.getDay()];
+let todayWeekNameEl = document.getElementById("todayWeekName");
+todayWeekNameEl.innerHTML = today;
+let min = now.getMinutes();
+if (min < 10) {
+  min = `0${min}`;
+}
+let time = `${now.getHours()}:${min}`;
+let currentTimeEl = document.getElementById("currentTime");
+currentTimeEl.innerHTML = time;
+
+let city = document.getElementById("searchForm");
+city.addEventListener("submit", search);
